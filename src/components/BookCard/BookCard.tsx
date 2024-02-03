e643132dc3883196916cefa88c887bfbc62bd2c6
Alex Volo <alex_volo@bk.ru>
2024-02-03 16:27:58 +0900

    feat: add card component and style

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -3,10 +3,12 @@ import S from "./BookCard.module.css";
 
 export const BookCard: React.FC<{ book?: Book }> = ({ book }) => {
   return (
-    <div className={S.card }>
-      <h2>{book?.title}</h2>
-      <p>Author: {book?.authors[0].name}</p>
-      <p>Subjects: {book?.subjects.join(" ")}</p>
+    <div className={S.card}>
+      <span className={S.delete}>X</span>
+      <h2 className={S.title}>{book?.title}</h2>
+      <p className={S.author}>Author: {book?.authors[0].name}</p>
+      <p className={S.subjects}>Subjects: {book?.subjects.join(" ")}</p>
+      <div className={S.like}>&#9825; &#9829;</div>
     </div>
   );
 };
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,12 +3,13 @@ import { BookCard } from "../../components/BookCard/BookCard";
 import S from "./Main.module.css";
 
 const cardsElements = books.map((book) => {
-  return <BookCard book={book} />;
+  return <BookCard key={book.id} book={book} />;
 });
 export const Main: React.FC = () => {
   return (
     <div className={S.wrapper}>
-      <h1 style={{ textAlign: "center" }}>Gutenberg Books</h1>
+      <h1 style={{ textAlign: "center", color: "#5f0421" }}>Gutenberg Books</h1>
+      <button className={S.filterButton}>Показать только понравившиеся</button>
       <div className={S.cards}>{cardsElements}</div>
     </div>
   );
