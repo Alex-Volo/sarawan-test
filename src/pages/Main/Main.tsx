5da684a209c754b03c042bf3495094951f1be4ac
Alex Volo <alex_volo@bk.ru>
2024-02-04 16:33:11 +0900

    feat: add filter by like

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,15 +1,19 @@
 import { BookCard } from "../../components/BookCard/BookCard";
 import S from "./Main.module.css";
 import { getBooks } from "../../service/api";
-import { useEffect } from "react";
-// import { IBook } from "../../types";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { booksSelector } from "../../store";
 import { setBooks } from "../../store/booksSlice";
 
 export const Main: React.FC = () => {
-  const books = useAppSelector(booksSelector);
+  let books = useAppSelector(booksSelector);
   const dispatch = useAppDispatch();
+  const [isFiltering, setIsFiltering] = useState<boolean>(false);
+
+  if (isFiltering) {
+    books = books.filter((book) => book.isLiked);
+  }
 
   const cardsElements = books.map((book) => {
     return <BookCard key={book?.id} book={book} />;
@@ -17,14 +21,21 @@ export const Main: React.FC = () => {
 
   useEffect(() => {
     getBooks().then((books) => {
-      dispatch(setBooks(books));
+      if (books) dispatch(setBooks(books));
     });
   }, [dispatch]);
 
   return (
     <div className={S.wrapper}>
       <h1 style={{ textAlign: "center", color: "#5f0421" }}>Gutenberg Books</h1>
-      <button className={S.filterButton}>Показать только понравившиеся</button>
+      <button
+        onClick={() => {
+          setIsFiltering(!isFiltering);
+          console.log("фильтруетсчя", isFiltering);
+        }}
+        className={S.filterButton}>
+        {isFiltering ? "Show all" : "Show only liked"}
+      </button>
       <div className={S.cards}>{cardsElements}</div>
     </div>
   );
--- src/service/api.ts
+++ src/service/api.ts
@@ -2,13 +2,12 @@ import { IBook } from "../types";
 
 const baseUrl = "https://gutendex.com/books/?page=2";
 
-export const getBooks = async () => {
+export const getBooks = async (): Promise<IBook[] | undefined> => {
   try {
     const response = await fetch(baseUrl);
     const json = await response.json();
     return json.results.map((el: IBook) => ({ ...el, isLiked: false }));
   } catch (error) {
     console.warn(error);
-    return error;
   }
 };
